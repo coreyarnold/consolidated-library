f146b685a83372b1a22caeb3ae9e159187c24886
Corey Arnold <coreyarnold@gmail.com>
2012-09-06 23:43:12 -0700

    VideoGames db install and initial shortcodes
    Got the custom table install for videogames working and made the
    initial shortcode. Added a backlog file with initial release line.

--- consolidated-library/consolidated-library.php
+++ consolidated-library/consolidated-library.php
@@ -8,24 +8,113 @@ Author: Corey Arnold
 Author URI: http://www.coreyarnold.com/
 */
 
-function consol_lib_printbooklist() {
-   echo "<p><b>Book List</b>&nbsp;";
-   echo "this is where we print the books";
-   echo "</p>";
+    register_activation_hook( __FILE__, 'consol_install');
+
+    function consol_install(){
+        global $wpdb;
+        $tablename = $wpdb->prefix . "consol_lib_videogames";
+        $sql = "CREATE TABLE `$tablename` (
+        `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+        `title` varchar(255) NOT NULL,
+        `platform` varchar(100) DEFAULT NULL,
+        `genre` varchar(100) DEFAULT NULL,
+        `singleplayer` tinyint(1) DEFAULT NULL,
+        `multiplayer` tinyint(1) DEFAULT NULL,
+        `multiplayerMaxPlayers` decimal(1,0) DEFAULT NULL,
+        `coop` tinyint(1) DEFAULT NULL,
+        `coopMaxPlayers` decimal(1,0) DEFAULT NULL,
+        `rating` decimal(2,0) DEFAULT NULL,
+        `purchase_price` decimal(5,2) DEFAULT NULL,
+        `purchase_date` date DEFAULT NULL,
+        `amazon_link` varchar(255) DEFAULT NULL,
+		`steam_appid` mediumint(9) DEFAULT NULL,
+        `notes` varchar(1000) DEFAULT NULL,
+        `status` enum ('Own it','Wishlist'),
+		`play_state` enum ('not started','not playing', 'playing', 'completed'),
+		`graphic_url` varchar(255) DEFAULT NULL,
+        `lastupdated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+        PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+		";
+		
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);        
+    }
+    
+// [videogames outputstyle="grid/list" platform="xbox360/ps3/steam/pc/mac" status="" rating="" coop=""]
+   add_shortcode( 'videogames', 'consol_lib_printvideogames');
+function consol_lib_printvideogames($atts) {
+	extract( shortcode_atts( array(
+	    'outputstyle' => "list",
+		'platform' => "all",
+		'status' => "all",
+		'rating' => "all",
+		'coop' => "all",
+	), $atts ) );
+
+       global $wpdb;
+//        $wpdb->show_errors();
+       $tablename = $wpdb->prefix . "consol_lib_videogames";
+
+	$filters = "";
+	if ($platform != "all")
+	{
+		if ($filters != "")
+			$filters .= " AND ";
+		$filters .= $wpdb->prepare("platform = %s",$platform);
+	}
+	if ($status != "all")
+	{
+		if ($filters != "")
+			$filters .= " AND ";
+		$filters .= $wpdb->prepare("status = %s",$status);
+	}	
+	if ($rating != "all")
+	{
+		if ($filters != "")
+			$filters .= " AND ";
+		$filters .= $wpdb->prepare("rating = %d",$rating);
+	}
+	if ($coop != "all")
+	{
+		if ($filters != "")
+			$filters .= " AND ";
+		$filters .= $wpdb->prepare("coop = %s",$coop);
+	}
+	if ($filters != "")
+		$filters = "WHERE $filters";
+       $sql = "select id, title, platform, genre, coop, coopMaxPlayers, rating, amazon_link from $tablename $filters order by title;";
+
+       $results = $wpdb->get_results($sql, ARRAY_A);
+	if ($outputstyle == "list")
+		videogames_print_list($results);
+	else if ( $outputstyle == "grid")
+		videogames_print_grid($results);
 }
 
-register_activation_hook(__FILE__,'my_first_install');
+function videogames_print_list($listresultset) {
+       echo "<p><b>Game List</b><br />";
+       foreach($listresultset as $game){
+           $title = $game['title'];
+           echo "Title: $title<br />";
+       }
+       
+       echo "</p>";
+}
 
-register_deactivation_hook( __FILE__, 'my_first_remove' );
+function videogames_print_grid($resultset) {
+       echo "<p><b>Game Grid</b><br />";
+       foreach($resultset as $game){
+           $title = $game['title'];
+           echo "Title: $title<br />";
+       }
+       
+       echo "</p>";
+}
 
-function my_first_install() {
    /* Creates new database field */
-   add_option("my_first_data", 'Testing !! My Plugin is Working Fine.', 'This is my first plugin panel data.', 'yes');
-}
+//   add_option("my_first_data", 'Testing !! My Plugin is Working Fine.', 'This is my first plugin panel data.', 'yes');
 
-function my_first_remove() {
-	delete_option( "my_first_data" );
-}
 
 /* Admin Panel */
 if ( is_admin() ){
