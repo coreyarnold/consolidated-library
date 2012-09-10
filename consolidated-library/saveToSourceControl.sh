d302962607a61b74978044d5dc0d6b31535491d5
Corey Arnold <coreyarnold@gmail.com>
2012-09-09 21:38:13 -0700

    add reverse rsync from source control

--- consolidated-library/saveToSourceControl.sh
+++ consolidated-library/saveToSourceControl.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 rsync -Cazv . ~/Documents/Repositories/GitHub/consolidated-library/consolidated-library
+
+#rsync -Cazv ~/Documents/Repositories/GitHub/consolidated-library/consolidated-library .
